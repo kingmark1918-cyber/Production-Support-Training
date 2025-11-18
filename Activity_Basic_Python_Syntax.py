66480d4497a9141bbff197ac088d95087b1ce82c
kingmark1918-cyber <king.mark1918@gmail.com>
2025-11-18 13:46:30 -0500

    Completed Python Script

---
+++ Activity_Basic_Python_Syntax.py
@@ -0,0 +1,56 @@
+# Activity 1
+from multiprocessing.reduction import duplicate
+
+# Block 1
+print("Python is fun!")
+
+# Block 1.2
+
+print("Python is fun");
+print("Python is also easy!");
+
+# Block 1.3
+
+(print
+ ("Python is fun!"));
+
+# Block 1.4
+
+first_name = "Rebecca"
+last_name = "Roberts"
+
+print(first_name)
+print(last_name)
+
+# Activity 2
+
+# Block 2.1
+# Add a new line of code that displays the text in quotation marks
+# to an output block without repeating the text in quotation marks.
+output = "I love Python!"
+duplicate_output = output
+
+print(output)
+print(duplicate_output)
+
+# Block 2.2
+# Display only the text "Python is fun!" to an output block without
+# deleting any of the existing code.
+print("Python is fun!")
+
+# THis would simply commenting all lines from 38 up or
+# using commenting block style.  I have not ran the program
+# and therefore indicate necessary steps to perform in lieu
+# of completing the steps.
+
+# Activity 3
+birth_place = input("Where is your birth place?")
+current_state = input("Where is your current state?")
+
+print(f"You was born in {birth_place} but live in {current_state}")
+print(current_state)
+
+# Activity 4
+title = "Pirates of the Caribbean"
+released_in = 2003
+print(f"{title}:{released_in}")
